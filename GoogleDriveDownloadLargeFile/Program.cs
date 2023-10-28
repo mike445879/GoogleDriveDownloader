57537da182e2bbd0efc7dcc87f8b00e2422de43d
oalabbasi <othmanalkhan@gmail.com>
2023-10-28 08:31:58 +0300

    Improved Download Progress Display
    Updated the download progress reporting mechanism to provide a more user-friendly display. The downloaded size is now presented in a readable format, switching between KB, MB, and GB units based on the size. This offers better clarity to the user regarding the download progress.

--- GoogleDriveDownloadLargeFile/Program.cs
+++ GoogleDriveDownloadLargeFile/Program.cs
@@ -14,98 +14,119 @@ namespace GoogleDriveDownloadLargeFile
     {
         static async Task Main(string[] args)
         {
-            // Replace with the path to your credentials file.
             string credentialsPath = @"C:\googledrivelargfile.json";
 
-            // Prompt the user to enter the file ID and output file name.
             Console.Write("Enter the file ID: ");
             string fileId = Console.ReadLine();
 
             Console.Write("Enter the output file name: ");
             string outputFileName = Console.ReadLine();
 
-            // Set the output file path to the Downloads folder.
             outputFileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads", outputFileName);
 
-            // Download the file and await its completion.
             await DownloadFileAsync(credentialsPath, fileId, outputFileName);
 
-            // Prompt the user to exit.
             Console.WriteLine("Download completed. Press enter to exit.");
             Console.ReadLine();
         }
 
-        // This method downloads a file from Google Drive using the Drive API.
         private static async Task DownloadFileAsync(string credentialsPath, string fileId, string outputFileName)
+        {
+            UserCredential credential = await GetUserCredentialAsync(credentialsPath);
+
+            using (var service = new DriveService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = credential
+            }))
+            {
+                var fileMetadata = GetFileMetadata(service, fileId);
+
+                if (fileMetadata != null)
+                {
+                    await DownloadFileContentAsync(service, fileId, outputFileName, fileMetadata);
+                }
+                else
+                {
+                    Console.WriteLine("File not found on Google Drive.");
+                }
+            }
+        }
+
+        private static async Task<UserCredential> GetUserCredentialAsync(string credentialsPath)
+        {
+            using (var stream = new FileStream(credentialsPath, FileMode.Open, FileAccess.Read))
+            {
+                return await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    new[] { DriveService.Scope.Drive },
+                    "user",
+                    CancellationToken.None);
+            }
+        }
+
+        private static Google.Apis.Drive.v3.Data.File GetFileMetadata(DriveService service, string fileId)
         {
             try
             {
-                // Load the user's credentials from the specified file path.
-                UserCredential credential;
-                using (var stream = new FileStream(credentialsPath, FileMode.Open, FileAccess.Read))
+                return service.Files.Get(fileId).Execute();
+            }
+            catch (Google.GoogleApiException ex)
+            {
+                Console.WriteLine($"Google API Error: {ex.Message}");
+                return null;
+            }
+        }
+
+        private static async Task DownloadFileContentAsync(DriveService service, string fileId, string outputFileName, Google.Apis.Drive.v3.Data.File fileMetadata)
+        {
+            var request = service.Files.Get(fileId);
+            var mediaDownloader = new MediaDownloader(service)
+            {
+                ChunkSize = 1024 * 1024 // 1 MB
+            };
+
+            long startByte = 0;
+            if (File.Exists(outputFileName))
+            {
+                var fileInfo = new FileInfo(outputFileName);
+                if (fileInfo.Length < fileMetadata.Size.GetValueOrDefault())
                 {
-                    credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
-                        GoogleClientSecrets.Load(stream).Secrets,
-                        new[] { DriveService.Scope.Drive },
-                        "user",
-                        CancellationToken.None);
-                }
-
-                // Create a new DriveService instance using the user's credentials.
-                var service = new DriveService(new BaseClientService.Initializer
-                {
-                    HttpClientInitializer = credential
-                });
-
-                // Create a new file download request using the specified file ID.
-                var request = service.Files.Get(fileId);
-
-                // Execute the file download request and get the file metadata.
-                var fileMetadata = request.Execute();
-
-                // Create a new MediaDownloader instance for downloading the file.
-                var mediaDownloader = new MediaDownloader(service);
-
-                // Set the starting byte position for resuming an interrupted download.
-                long startByte = 0;
-                if (File.Exists(outputFileName))
-                {
-                    var fileInfo = new FileInfo(outputFileName);
                     startByte = fileInfo.Length;
                 }
+                else
+                {
+                    Console.WriteLine("Download already seems to be complete.");
+                    return;
+                }
+            }
 
-                // Open the output file stream for writing and set the download chunk size.
+            mediaDownloader.ProgressChanged += (IDownloadProgress progress) =>
+            {
+                switch (progress.Status)
+                {
+                    case DownloadStatus.Downloading:
+                        string formattedSize = FormatSize(progress.BytesDownloaded);
+                        Console.Write($"\rDownloaded {formattedSize}.");
+                        break;
+                    case DownloadStatus.Completed:
+                        Console.WriteLine("\rDownload completed.                  ");
+                        break;
+                    case DownloadStatus.Failed:
+                        Console.WriteLine($"\rDownload failed: {progress.Exception}");
+                        break;
+                }
+            };
+
+            try
+            {
                 using (var fileStream = new FileStream(outputFileName, FileMode.OpenOrCreate))
                 {
-                    // Configure the chunk size of the media downloader.
-                    //mediaDownloader.ChunkSize = 256 * 1024; // 256KB
-                    mediaDownloader.ChunkSize = 1024 * 1024; // 1 MB
-
-                    // Set up the progress changed event handler.
-                    mediaDownloader.ProgressChanged += (IDownloadProgress progress) =>
-                    {
-                        switch (progress.Status)
-                        {
-                            case DownloadStatus.Downloading:
-                                Console.WriteLine($"Downloaded {progress.BytesDownloaded} bytes.");
-                                break;
-                            case DownloadStatus.Completed:
-                                Console.WriteLine("Download completed.");
-                                break;
-                            case DownloadStatus.Failed:
-                                Console.WriteLine($"Download failed: {progress.Exception}");
-                                break;
-                        }
-                    };
-
-                    // If the file exists, set the media downloader's range header and seek the file stream.
                     if (startByte > 0)
                     {
                         mediaDownloader.Range = new RangeHeaderValue(startByte, null);
                         fileStream.Seek(startByte, SeekOrigin.Begin);
                     }
 
-                    // Download the file asynchronously using the media downloader and output file stream.
                     var downloadUri = request.CreateRequest().RequestUri.ToString();
                     await mediaDownloader.DownloadAsync(downloadUri, fileStream);
                 }
@@ -113,8 +134,27 @@ namespace GoogleDriveDownloadLargeFile
             catch (Exception ex)
             {
                 Console.WriteLine($"An error occurred: {ex.Message}");
+                File.Delete(outputFileName); // Cleanup partially downloaded file
             }
         }
+        private static string FormatSize(long bytes)
+        {
+            const double KB = 1024;
+            const double MB = KB * 1024;
+            const double GB = MB * 1024;
 
+            if (bytes < MB)
+            {
+                return $"{bytes / KB:F2} KB";
+            }
+            else if (bytes < GB)
+            {
+                return $"{bytes / MB:F2} MB";
+            }
+            else
+            {
+                return $"{bytes / GB:F2} GB";
+            }
+        }
     }
 }
